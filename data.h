f630179be8ec89ee487240679e1a69a7095f2d94
arantxa <arantxagarcia337@gmail.com>
2024-10-25 20:37:43 -0300

    Update data.h

--- data.h
+++ data.h
@@ -1,5 +1,5 @@
 // contraseñas de los multi wifi
-const char* ssid_1 = "Fibertel Wifi352";
+const char* ssid_1 = "Fibertel WiFi352";
 const char* psswrd_1 = "0049353561";
 const char* ssid_2 = "estudiantes";
 const char* psswrd_2 = "educar_2018";
